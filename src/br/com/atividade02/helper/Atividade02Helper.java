7e01782f7521f2a0e182b7229ce6394fd1ddb792
silasianez <silas.ianez@techne.com.br>
2017-06-03 13:20:19 -0300

    implementação de consultas

--- src/br/com/atividade02/helper/Atividade02Helper.java
+++ src/br/com/atividade02/helper/Atividade02Helper.java
@@ -1,9 +1,17 @@
 package br.com.atividade02.helper;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import br.com.atividade02.entity.Clientes;
 import br.com.atividade02.entity.Pedidos;
+
 
 
 public class Atividade02Helper {
@@ -36,5 +44,25 @@ public class Atividade02Helper {
 		} catch (Exception e) {
 			return e.getMessage();
 		}
+	}
+	
+	//JPQL: Usando Query
+	@SuppressWarnings("unchecked")
+	public List<Clientes> listarClientes(){
+		TypedQuery<Clientes> tQuery = em.createQuery("select c from Clientes c", Clientes.class); 
+		return tQuery.getResultList();
+	}	
+	
+	//Busca o cliente por ID
+	public Clientes findOnecliente(Integer idcliente){		
+		TypedQuery<Clientes> tQuery = em.createQuery("select c from Clientes c where idcliente = :idcliente", Clientes.class);
+		tQuery.setParameter("idcliente", idcliente); 
+		return tQuery.getSingleResult(); 	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Pedidos> listarPedidos(){
+		TypedQuery<Pedidos> tQuery = em.createQuery("select p from Pedidos p", Pedidos.class); 
+		return tQuery.getResultList();
 	}
 }
--- src/br/com/atividade02/helper/Main.java
+++ src/br/com/atividade02/helper/Main.java
@@ -1,38 +1,74 @@
 package br.com.atividade02.helper;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import br.com.atividade02.entity.Clientes;
 import br.com.atividade02.entity.Pedidos;
 
-public class Main {
 
+public class Main {
+	
 	public static void main(String[] args) {
 		
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("PersistAtividade02"); 
-		
-		EntityManager em = entityManagerFactory.createEntityManager();
+		EntityManager em = entityManagerFactory.createEntityManager();		
+
 		
 		Atividade02Helper fHelper = new Atividade02Helper(em);
 		
 		Clientes clientes = new Clientes();
-		clientes.setNome("Silas Ianez");
-		clientes.setEmail("Silas@gmail.com");
+		clientes.setNome("Papai noel nogueira");
+		clientes.setEmail("Papainoel@gmail.com");
 		
 		Pedidos pedidos = new Pedidos();
         pedidos.setData(new Date());
-        pedidos.setDescricao("Caderno do batman");
-        pedidos.setValor(10.98);
+        pedidos.setDescricao("trenó para 8 renas");
+        pedidos.setValor(1987.98);
 		
 		System.out.println(clientes.getIdcliente());
 		
         fHelper.salvar(clientes);
 		fHelper.vincularPedido(clientes.getIdcliente(), pedidos);
 
+		System.out.println("==============Lista dos clientes==============");
+		listarClientes(em);
+
+		System.out.println("==============Recupera um único cliente==============");
+		findOnecliente(em, 1);
+		
+		System.out.println("==============Lista de pedidos==============");
+		listarPedidos(em);
+		
 	}
+
+	private static void listarClientes(EntityManager em){ 
+		Atividade02Helper dao = new Atividade02Helper(em);
+		List<Clientes> clientes = dao.listarClientes(); 
+		for (Clientes cliente : clientes) {
+			System.out.println(cliente.getNome() + " - " + cliente.getEmail());
+		}
+	}
+	
+	private static void findOnecliente(EntityManager em, Integer idcliente){ 
+		Atividade02Helper dao = new Atividade02Helper(em);
+		Clientes f = dao.findOnecliente(idcliente); 
+		System.out.println(f.getNome() + ": " + f.getEmail());
+	} 
+	
+	private static void listarPedidos(EntityManager em){ 
+		Atividade02Helper dao = new Atividade02Helper(em);
+		List<Pedidos> pedidos = dao.listarPedidos(); 
+		for (Pedidos pedido : pedidos) {
+			System.out.println("Pedido do cliente: " + pedido.getClientes().getNome() + " - " + pedido.getDescricao() + " no valor de " + pedido.getValor() );
+		}
+	}
+		
+	
 
 }
